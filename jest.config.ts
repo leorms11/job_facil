b121d26e2528af33bb8b1b5ce8b04452f596375e
Leonardo <leonardo.rms11@gmail.com>
2022-05-19 09:40:26 -0300

    utilização do swc ao invés do ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,33 @@ export default {
   clearMocks: true,
   coverageProvider: "v8",
   testMatch: ["**/*.spec.ts"],
-  preset: "ts-jest",
+  // preset: "ts-jest",
   bail: true,
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
   }),
+  transform: {
+    "^.+\\.(t|j)s$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: false,
+            decorators: true,
+          },
+          target: "es2017",
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+        },
+        module: {
+          type: "es6",
+          noInterop: false,
+        },
+      },
+    ],
+  },
 };
